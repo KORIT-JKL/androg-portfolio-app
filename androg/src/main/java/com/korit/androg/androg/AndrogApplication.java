2fe280ea3a25ad3938cf7f4c5c5edbf3b2d10cc7
Kanguijin <wheretogobro@naver.com>
2023-05-02 14:30:57 +0900

    init: 프로젝트 초기 세팅

---
+++ androg/src/main/java/com/korit/androg/androg/AndrogApplication.java
@@ -0,0 +1,13 @@
+package com.korit.androg.androg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndrogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AndrogApplication.class, args);
+	}
+
+}
---
+++ androg/src/test/java/com/korit/androg/androg/AndrogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.korit.androg.androg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AndrogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
