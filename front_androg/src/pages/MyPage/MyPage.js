d9d348565f76685f82dda8a215d9eecca61ce9c2
otso <hhgg0017@gmail.com>
2023-05-12 00:42:02 +0900

    feat: 구매한 상품 목록 front 구현

--- front_androg/src/pages/MyPage/MyPage.js
+++ front_androg/src/pages/MyPage/MyPage.js
@@ -31,6 +31,7 @@ const myInfoContent = css`
   margin-bottom: 100px;
 `;
 const Title = css`
+  margin-top: 5px;
   font-size: 25px;
   font-weight: 600;
   padding-bottom: 20px;
@@ -74,6 +75,77 @@ const orderContent = css`
   align-items: center;
   margin-bottom: 10px;
   max-height: 100%;
+`;
+
+const orderbox = css`
+  width: 1030px;
+  height: 720px;
+  overflow-y: auto;
+`;
+
+const orderDetailBox = css`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border: 1px solid black;
+  margin: 10px 0px 10px 2px;
+  padding: 5px;
+  width: 70%;
+  height: 200px;
+`;
+
+const orderImgBox = css`
+  border: 1px solid black;
+  padding: 5px;
+  width: 30%;
+  height: 100%;
+  overflow: hidden;
+`;
+
+const orderImg = css`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`;
+
+const orderInfoBox = css`
+  border: 1px solid black;
+  padding: 5px;
+  width: 35%;
+  height: 100%;
+`;
+
+const productName = css`
+  margin-top: 5px;
+  font-size: 25px;
+  font-weight: 600;
+  padding-bottom: 10px;
+`;
+
+const orderInfoText = css`
+  font-size: 20px;
+  font-weight: 600;
+  margin-bottom: 10px;
+`;
+
+const reviewButtonBox = css`
+  padding: 5px;
+  width: 30%;
+  height: 100%;
+`;
+
+const reviewButton = css`
+  font-weight: 600;
+  font-size: 17px;
+  height: 100%;
+  width: 100%;
+  background-color: black;
+  color: white;
+  cursor: pointer;
+  &:hover {
+    background-color: grey;
+    color: white;
+  }
 `;
 
 const MyPage = () => {
@@ -133,6 +205,30 @@ const MyPage = () => {
         </div>
         <div css={orderContent}>
           <h2 css={Title}>주문 기록</h2>
+          <div css={orderbox}>
+            <div css={orderDetailBox}>
+              <div css={orderImgBox}>
+                <img
+                  css={orderImg}
+                  src="//cdn.shopify.com/s/files/1/0099/5708/1143/products/1994916_CORA_1_476ac993-fee8-47a7-b61c-8419f9fcc0e0.jpg?v=1683052708&width=480"
+                  alt=""
+                />
+              </div>
+              <div css={orderInfoBox}>
+                <h2 css={productName}>GOLD LION LS TEE</h2>
+                <p css={orderInfoText}>
+                  옵션 : coral/M <br />
+                </p>
+                <p css={orderInfoText}>
+                  수량 : 1 <br />
+                </p>
+                <p css={orderInfoText}>가격 : ₩82000</p>
+              </div>
+              <div css={reviewButtonBox}>
+                <button css={reviewButton}>리뷰 등록</button>
+              </div>
+            </div>
+          </div>
         </div>
       </main>
       <CommonFooter />
